1ce5061280fadb3a44f78071df139ef88fc87cc3
Munyoung Hwang <munyoung.hwang@navercorp.com>
2019-04-05 00:47:54 +0900

    add keybinding C-' (evil-switch-to-windows-last-buffer)

--- evil-key.el
+++ evil-key.el
@@ -5,3 +5,5 @@
   (kbd ", g d") 'helm-gtags-find-tag-from-here
   (kbd ", g n") 'helm-gtags-next-history
   (kbd ", g p") 'helm-gtags-previous-history)
+
+(global-set-key (kbd "C-'") 'evil-switch-to-windows-last-buffer)
